782fb871c4010d584fbd23e9a437bae2308cfdd5
SangDon <adoim@naver.com>
2023-11-30 01:04:33 +0900

    Feat: Simple version of testing

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,10 +2,10 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 
-interface UiProviderProps {
+type UiProviderProps = {
   children: React.ReactNode;
   className?: string;
-}
+};
 
 export function Providers({ children, className }: UiProviderProps) {
   return <NextUIProvider className={className}>{children}</NextUIProvider>;
